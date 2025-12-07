f75a9851303045a726be5de88bb322b6a0e0a110
Teddie Finström <teddie.finstrom@outlook.com>
2025-12-07 14:11:46 +0100

    Added search bar to recipe page. Will add filtering bttons next.

--- src/pages/RecipePage.tsx
+++ src/pages/RecipePage.tsx
@@ -2,9 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import RecipeCard from "../components/RecipeCard";
 import { getAllRecipes } from "../service/mealDB";
 import { useEffect, useMemo, useRef, useState } from "react";
+import SearchBar from "../components/SearchBar";
 
 const RecipePage = () => {
   const [visibleCount, setVisibleCount] = useState(20);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const { data: allRecipes } = useQuery({
     queryKey: ["all-recipes"],
@@ -18,30 +20,41 @@ const RecipePage = () => {
     return [...allRecipes].sort(() => Math.random() - 0.5);
   }, [allRecipes]);
 
+  const filteredRecipies = useMemo(() => {
+    if (!searchQuery) return randomRecipe;
+
+    return randomRecipe.filter((recipe) =>
+      recipe.strMeal.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }, [searchQuery, randomRecipe]);
+
+  const selectedRecipes = searchQuery ? filteredRecipies : randomRecipe;
+
+  const visibleRecipes = selectedRecipes.slice(0, visibleCount);
+
   useEffect(() => {
     if (!loadMoreRef.current) return;
 
-    const observer = new IntersectionObserver(
-      (entries) => {
-        const target = entries[0];
-        if (target.isIntersecting) {
-          setVisibleCount((prev) => prev + 20);
-        }
-      },
-      { threshold: 1 }
-    );
+    const observer = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+            setVisibleCount((prev) => prev + 20);
+          }
+        });
 
     observer.observe(loadMoreRef.current);
 
     return () => observer.disconnect();
   }, []);
 
-  const visibleRecipes = randomRecipe.slice(0, visibleCount);
+
 
   return (
     <>
       <div className="recipe-hero">
-        <h1>Recipes</h1>
+        <div className="hero-content">
+          <h1>Recipes</h1>
+          <SearchBar onSearch={(value) => setSearchQuery(value)} />
+        </div>
       </div>
       <div className="recipe-page">
         <div className="recipe-grid">
